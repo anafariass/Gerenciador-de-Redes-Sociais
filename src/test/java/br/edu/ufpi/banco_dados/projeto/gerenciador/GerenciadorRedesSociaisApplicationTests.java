ab1930fd3a1d112cf3d6c35a1ac5a9327235ac24
anafariass <livia.oliveira1.only@gmail.com>
2025-08-04 17:09:42 -0300

    Inicializa o projeto Gerenciador de Redes Sociais com configuração do Maven e estrutura básica

---
+++ src/test/java/br/edu/ufpi/banco_dados/projeto/gerenciador/GerenciadorRedesSociaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufpi.banco_dados.projeto.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorRedesSociaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
